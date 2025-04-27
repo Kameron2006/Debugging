748861256183adf3c391e3bbb4720d2eb429c5bc
Kameron H <kam.harvey10@gmail.com>
2025-04-27 01:56:09 -0400

    Initial Commit

---
+++ Debugging/DebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingApp.swift
+//  Debugging
+//
+//  Created by Scholar on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DebuggingTests/DebuggingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingTests.swift
+//  DebuggingTests
+//
+//  Created by Scholar on 4/27/25.
+//
+
+import Testing
+@testable import Debugging
+
+struct DebuggingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
